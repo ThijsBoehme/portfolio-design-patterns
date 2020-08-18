6f8d07cb44a182d10a582d7c0f40a9fe74ded08e
Thijs Boehme <thijs.boehme@qmino.com>
2020-08-18 10:47:44 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("jvm") version "1.4.0"
+}
+
+group = "com.thijsboehme"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.reflections:reflections:0.9.12")
+    implementation("org.apache.commons:commons-lang3:3.11")
+    implementation("com.google.guava:guava:29.0-jre")
+    implementation("junit:junit:4.13")
+}
